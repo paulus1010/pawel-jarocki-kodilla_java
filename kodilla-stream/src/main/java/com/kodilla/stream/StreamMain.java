2de6041e40d979cd0e90676ee4ee78761ee30389
paulus1010 <dev@mikrolila.vxm.pl>
2022-01-21 18:55:38 +0100

    Stan po korekcie build.gradle w katalogu głównym projektu

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,12 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.lambda.SaySomething;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to module 7 - Stream");
+
+        SaySomething saySomething = new SaySomething();
+        saySomething.say();
+    }
+}
